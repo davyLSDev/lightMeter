c2393ef6db37829254fb7239ea9987b394e02e47
Davy Tenn <davy.lsdev@gmail.com>
2019-04-20 21:49:08 -0600

    Refactor get pot value and calculate ev

--- lib/hardware.h
+++ lib/hardware.h
@@ -16,6 +16,36 @@ int getLightReading (float filmSpeed, float aperture, float shutterSpeed, float 
 }
 
 /*********************************************
+ * Calibrated light reading based on 10k
+ * resistor across the solar cell, using
+ * a standard photographic light meter for
+ * the EV reading at 100 ASA.
+ *
+ * EV LR (Light reading)
+ * 4  45
+ * 5  58Begin
+ * 6  76
+ * 7  113
+ * 8  176
+ * 9  192
+ * 10 205
+ * 11 293
+ * 12 330
+ * 13 425
+ * 14 457
+ * 15 470
+ * clearLcdChars (lineOneEraseFromPosition, 1);
+ * Entering these numbers into
+ * https://www.easycalculation.com/analytical/least-squares-regression-line-equation.php
+ * generated the formula to calculate EV based on
+ * the light reading.
+ *********************************************/
+float evCalibrated (float lightReading) {
+  float result;
+  return result = 0.023*lightReading + 4.107;
+}
+
+/*********************************************
  * get the variable resistor value
  *********************************************/
 float getVariableResistorValue () {
